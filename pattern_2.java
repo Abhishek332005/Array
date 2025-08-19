b7e5852162c54a0dfb5ee339444619812bf13992
Abhishek Pratap Singh <ytabhishek721@gmail.com>
2025-08-20 00:04:55 +0530

    Add files via upload

---
+++ pattern_2.java
@@ -0,0 +1,20 @@
+package Array_Exercise;
+import java.util.Scanner;
+public class pattern_2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the number of rows: ");
+        int r = sc.nextInt();
+
+        System.out.println("Enter the number of columns: ");
+        int c = sc.nextInt();
+
+        for (int i = 1; i <= r; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+}
